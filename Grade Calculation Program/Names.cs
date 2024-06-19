53676800a060cfd28ad76c0e281290999e75a492
ilyas <ilyas.imamoglu.ii@gmail.com>
2024-06-19 15:05:00 +0300

    Add project files.

---
+++ Grade Calculation Program/Names.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Calculation_Program
+{
+    internal class Names
+    {
+       public string[] name = new string[2];
+       public int[] point = new int[2];
+        
+
+
+       public void isimler()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter Name ::");
+                name[i] = Console.ReadLine();
+
+
+            }
+        }
+
+        public void puanlar() 
+        {
+            int ort = 0;
+            for(int j = 0;j < 2;j++)
+            {
+                Console.WriteLine("Enter Grade ::");
+                point[j] = Convert.ToInt32(Console.ReadLine());
+                ort += point[j];
+            
+             
+                
+
+            }
+            int tt = ort / 2;
+            Console.WriteLine("The Avrage is ::" + tt);
+            for (int j = 0; j <= 2; j++)
+            {
+                if (point[j] >= tt)
+                {
+                    Console.WriteLine(name[j] + "::pass !!!");
+                    Console.WriteLine("The Grade is ::" + point[j]);
+
+                }
+                else
+                {
+                    Console.WriteLine(name[j] + "::faild !!!");
+                    Console.WriteLine("The Grade is ::" + point[j]);
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Grade Calculation Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Calculation_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //20 kişilik bir sınıfta her öğrencini adı ve notu girelcek.
+            //sınıf ortalması hesaplancaktır ortalmanın altında notu olan öğrence KALMIŞ,
+            // eşit ve üzerei olan öğrenci geçmiştir 
+            // her öğrencini adını ve geçme durmunu yapcak bir console.app 
+            
+            
+           Names bubi = new Names();
+            bubi.isimler();
+            bubi.puanlar();
+
+
+            Console.ReadLine();
+        }
+    }
+}
